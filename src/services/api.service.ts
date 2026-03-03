c041b2c76c7fbe6c6551e5bf9e2d8040a4528d50
emilynvs <emilynascimento22@outlook.com>
2026-03-03 10:07:14 -0300

    wip

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,12 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
+export interface Professores {
+  id: number;
+  nome: string;
+  house: string;
+  image: string;
+  patronus: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
-
 export class Api {
-private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
+  private api = 'https://hp-api.onrender.com/';
 
+  getProfessores() {
+    return this.http.get<Professores>;
+  }
 }
